8e272cd075e2cbabe55dfa3bbc241ab914495ba4
Kuwamura <jobtech.kumamura@gmai.com>
2022-08-28 15:20:16 +0900

    Add.

---
+++ JavaScript.js
@@ -0,0 +1,33 @@
+'use strict'
+const button = document.getElementById("output");
+const fizzbuzzList = document.getElementById("fizzbuzzList");
+
+const listAdd = (i) => {
+  const li = document.createElement("li");
+  li.innerHTML = i;
+  fizzbuzzList.appendChild(li);
+};
+
+button.addEventListener("click", () => {
+  let fizzNum = Number(document.getElementById("fizzNum").value);
+  let buzzNum = Number(document.getElementById("buzzNum").value);
+
+  if (
+    Number.isInteger(fizzNum) &&
+    Number.isInteger(buzzNum) &&
+    fizzNum > 0 &&
+    buzzNum > 0
+  ){
+    for (let i = 1; i < 100; i++) {
+      if (i % fizzNum === 0 && i % buzzNum === 0) {
+        listAdd("FizzBuzz" + i);
+      } else if (i % fizzNum === 0) {
+        listAdd("Fizz" + i);
+      } else if (i % buzzNum === 0) {
+        listAdd("Buzz" + i);
+      }
+    }
+  } else {
+    listAdd("整数値を入力してください");
+  }
+});
